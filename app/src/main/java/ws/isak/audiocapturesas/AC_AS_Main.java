4380b65b3d727e28b245730eb1b8326473aa99b1
Isak Herman <isak@alumni.stanford.edu>
2016-07-05 14:46:31 +0100

    first commit

---
+++ app/src/main/java/ws/isak/audiocapturesas/AC_AS_Main.java
@@ -0,0 +1,13 @@
+package ws.isak.audiocapturesas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AC_AS_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ac__as__main);
+    }
+}
