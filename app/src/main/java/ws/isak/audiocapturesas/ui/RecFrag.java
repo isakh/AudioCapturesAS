24356e2fcefe25460ff3009157588485433c72d4
Isak Herman <isak@alumni.stanford.edu>
2016-08-15 14:26:29 +0100

    updates in RecFrag to storageDir, testFileDir, and testFileName methods which correctly construct the directory structure for storing recording files

--- app/src/main/java/ws/isak/audiocapturesas/ui/RecFrag.java
+++ app/src/main/java/ws/isak/audiocapturesas/ui/RecFrag.java
@@ -37,19 +37,24 @@ import java.util.Date;
  */
 public class RecFrag extends Fragment implements View.OnClickListener {
 
-    Button record, stop, play, save;        //Control buttons
+
+    private Button record, stop, play, save;        //Control buttons
 
     private AudioRecord audioRec = null;    //MediaRecorder object triggered during record
 
-    private File fieldRecordingFile;      //File to hold a new field recording - there can only be one at a time
-
     private String storageState;            //the state of external storage as returned by environment
     private String storageDirectory;        //set/get by methods, set as composite of user and path variables
-    //private String outputFileName;        //TODO initially set as strings.xml TestFile, need to make naming dynamic
 
-    private String timeStamp;
+    private File storageDir;                //File used to build directory path to FieldRecordings
+    private boolean isStorageDirMade;
+    private File fieldRecordingDir;         //File for newFieldRecording directory - there can only be one at a time
+    private boolean isFieldRecordingDirMade;
+    private File fieldRecordingFile;        //File for newFieldRecording - this is where the wav data is written
+    private boolean isFieldRecordingFileMade;
 
-    private int bufferSize;             //buffer for storing live audio input for processing prior to writing to file
+    private String timeStamp;               //used in constructing newFieldRecording directory name
+
+    private int bufferSize;                 //buffer for storing live audio input for processing prior to writing to file
     private Thread recordingThread = null;  //initialize to null
     private boolean isRecording = false;    //initialize to false
 
@@ -79,19 +84,23 @@ public class RecFrag extends Fragment implements View.OnClickListener {
     public void onClick(View v) throws IllegalArgumentException, SecurityException, IllegalStateException {
         switch (v.getId()) {
             case R.id.recButtonRecFrag:
+                System.out.println("RecFrag: onClick: Record Button Pressed");
                 Toast.makeText(getActivity(), "Record Button Pressed", Toast.LENGTH_SHORT).show();
                 startRecording();
                 setButtonsStates(false, true, false, false);
                 break;
             case R.id.stopButtonRecFrag:
+                System.out.println("RecFrag: onClick: Stop Button Pressed");
                 Toast.makeText(getActivity(), "stopButton: Stop Button Pressed", Toast.LENGTH_SHORT).show();
                 stopRecording();
                 setButtonsStates(true, false, true, true);
                 break;
             case R.id.playButtonRecFrag:
+                System.out.println("RecFrag: onClick: Play Button Pressed");
                 Toast.makeText(getActivity(), "Play Button Pressed", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.saveButtonRecFrag:
+                System.out.println("RecFrag: onClick: Save Button Pressed");
                 Toast.makeText(getActivity(), "Save Button Pressed", Toast.LENGTH_SHORT).show();
                 //TODO copy contents of record buffer to library file for long term storage/manipulation
                 //createFieldRecordingFile();
@@ -104,11 +113,12 @@ public class RecFrag extends Fragment implements View.OnClickListener {
     their on-click listeners for further interaction.
      */
     private void setButtonHandlers(View v) {
+        System.out.println("RecFrag: setButtonHandlers: creating Buttons");
         record = (Button) v.findViewById(R.id.recButtonRecFrag);
         stop = (Button) v.findViewById(R.id.stopButtonRecFrag);
         play = (Button) v.findViewById(R.id.playButtonRecFrag);
         save = (Button) v.findViewById(R.id.saveButtonRecFrag);
-
+        System.out.println("RecFrag: setButtonHandlers: setting on-click listeners");
         record.setOnClickListener(this);
         stop.setOnClickListener(this);
         play.setOnClickListener(this);
@@ -117,8 +127,9 @@ public class RecFrag extends Fragment implements View.OnClickListener {
 
     /*
     A private function that sets the initial state of the buttons suitable for the interaction
-     */
+    */
     private void setButtonsStates(boolean recState, boolean stopState, boolean playState, boolean saveState) {
+        System.out.println("RecFrag: setButtonStates: buttons states set");
         record.setEnabled(recState);
         stop.setEnabled(stopState);
         play.setEnabled(playState);          //if this is to be true, we would need library access (jump to frag)
@@ -128,36 +139,66 @@ public class RecFrag extends Fragment implements View.OnClickListener {
     /*
     Private functions that set and get up the external storage directory where data is to be collected
     once it has been saved. (TODO serialize all data ??)
-    Check the state of the external storage location to verify it's availability
+    Check the state of the external storage location to verify it's availability.  This function only
+    creates the folder structure to the point of ~/storage/sdcard/FieldRecordings (or variation thereof.)
+    The subsequent subfolder for the data collected at a given point in time will be created in
+    another function.
      */
     private void setExternalStorageDir () {
         storageState = android.os.Environment.getExternalStorageState();
-        System.out.println("storageState is: " + storageState);
-        //in case of problem, notify user that storage is not available
+        System.out.println("RecFrag: setExternalStorageDir: storageState is: " + storageState);
+        //in case of mounting problem, notify user that storage is not available
         if (!storageState.equals(Environment.MEDIA_MOUNTED)) {
+            //send error message to logcat
+            System.out.println("RecFrag: setExternalStorageDir: ERROR: External Storage (SD CARD) Is Not Mounted. It is " + storageState + ".");
+            //and also to user on device
             Toast.makeText(getActivity(),
                     "ERROR: External Storage (SD CARD) Is Not Mounted. It is " + storageState + ".",
                     Toast.LENGTH_SHORT).show();
         }
         //Define the storage directory we want to use and check that it exists
-        storageDirectory = Environment.getExternalStorageDirectory()
+        storageDirectory = Environment.getExternalStorageDirectory().getAbsolutePath()
                 + File.separator
                 + getActivity().getString(R.string.storage_directory)
                 + File.separator;
-        System.out.println("Storage Directory: " + storageDirectory);
+        System.out.println("RecFrag: setExternalStorageDir: Environment.getExternalStorageDirecory().getAbsolutePath() is :"
+                + Environment.getExternalStorageDirectory().getAbsolutePath());
+        storageDir = new File(storageDirectory);
+        isStorageDirMade = storageDir.mkdirs();
+        if (storageDir.exists()) {
+            System.out.println("RecFrag: setExternalStorageDir: storageDir exists is: " + storageDir.exists());
+        }
+        if (!isStorageDirMade) {
+            System.out.println("RecFrag: setExternalStorageDir: failed to make storageDir");
+        } else {
+            try {
+                String path = storageDir.getCanonicalPath();
+                System.out.println("RecFrag: setExternalStorageDir: Storage Directory is: " + path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (SecurityException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
-    private String getExternalStorageDir () {
-        return storageDirectory;
+    /*
+    A private method that returns the File object (for the Directory) at storageDir
+     */
+    private File getExternalStorageDir () {
+        System.out.println("RecFrag: getExternalStorageDir: returning storage directory");
+        return storageDir;
     }
+
 
     /*
     A private method that returns the time stamp of the current time as a string.  This is to be
     used as a UID for file creation even if the user doesn't provide additional unique information
     */
     private static String getCurrentTimeStamp() {
+        System.out.println("RecFrag: getCurrentTimeStamp: initialized");
         try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HHmmss");
             String currentTimeStamp = dateFormat.format(new Date());
             return currentTimeStamp;
         } catch (Exception e) {
@@ -174,32 +215,65 @@ public class RecFrag extends Fragment implements View.OnClickListener {
     unless each user has UID]
     */
     private void setFieldRecordingFile () {
+        System.out.println("RecFrag: setFieldRecordingFile: initialized");
         timeStamp = getCurrentTimeStamp();
-        System.out.println("Timestamp is: " + timeStamp);
+        System.out.println("RecFrag: setFieldRecordingFile: Timestamp is: " + timeStamp);
 
-        File fieldRecordingFile = new File(storageDirectory
-                + getActivity().getString(R.string.test_file_name)
+        fieldRecordingDir = new File(storageDir,
+                getActivity().getString(R.string.test_file_folder)
+                + timeStamp);
+        isFieldRecordingDirMade = fieldRecordingDir.mkdirs();
+        if (fieldRecordingDir.exists()) {
+            System.out.println("RecFrag: setFieldRecordingFile: fieldRecordingDir exists is: " + fieldRecordingDir.exists());
+        }
+        if (!isFieldRecordingDirMade) {
+            System.out.println("RecFrag: setFieldRecordingFile: failed to make fieldRecordingDir");
+        }
+        else {
+            try {
+                String path = fieldRecordingDir.getCanonicalPath();
+                System.out.println("RecFrag: setFieldRecordingFile: Field Recording Directory is: " + path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch  (SecurityException ex) {
+                ex.printStackTrace();
+            }
+        }
+        fieldRecordingFile = new File (fieldRecordingDir,
+                getActivity().getString(R.string.test_file_name)
                 + "_"
                 + timeStamp
                 + getActivity().getString(R.string.audio_file_format));
-
+        isFieldRecordingFileMade = fieldRecordingFile.mkdir();
         if (fieldRecordingFile.exists()) {       //Tell this to the user so keep as Toast
-            Toast.makeText(getActivity(),
-                    "onCreate: newFiledRecordingFile ALREADY EXISTS... Overwrite?",
-                    Toast.LENGTH_LONG).show();
-        } else {
-            System.out.println("Creating File: " + storageDirectory + getActivity().getString(R.string.test_file_name) + "_" + timeStamp + getActivity().getString(R.string.audio_file_format));
+            System.out.println("RecFrag: setFieldRecordingFile: fieldRecordingFile exists is: " + fieldRecordingFile.exists());
+        }
+        if (!isFieldRecordingFileMade){
+            System.out.println("RecFrag: setFieldRecordingFile: failed to make fieldRecordingFile");
+        }
+        else {
             try {
-                fieldRecordingFile.createNewFile();
+                String path = fieldRecordingFile.getCanonicalPath();
+                System.out.println("RecFrag: setFieldRecordingFile: Field Recording File is: " + path);
             } catch (IOException e) {
                 e.printStackTrace();
-                ;
+            } catch (SecurityException e) {
+                e.printStackTrace();
             }
         }
     }
 
     private String getFieldRecordingFileName () {
-        return fieldRecordingFile.getName();
+        try {
+            System.out.println("RecFrag: getFieldRecordingFileName: file name is: " + fieldRecordingFile.getCanonicalPath());
+            return fieldRecordingFile.getCanonicalPath();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (SecurityException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     /*
@@ -243,11 +317,13 @@ public class RecFrag extends Fragment implements View.OnClickListener {
     TODO need to figure out how this file relates to the permanent one in external storage
      */
     private void writeAudioDataToFile () {
+        System.out.println("RecFrag: writeAudioDataToFile: initializing");
         short sData[] = new short [ConfigurationParameters.BUFFER_ELEMENTS_TO_RECORD];
 
         FileOutputStream outputStream = null;
         try {
             setFieldRecordingFile();
+            System.out.println("RecFrag: writeAudioDataToFile: called setFieldRecordingFile()");
             outputStream = new FileOutputStream(getFieldRecordingFileName());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -279,6 +355,7 @@ public class RecFrag extends Fragment implements View.OnClickListener {
     constructed to read in the data from audioRec into a bData array of bytes for output writen to file.
      */
     private byte[] shortToByte (short[] sData) {
+        System.out.println("RecFrag: shortToByte: initializing");
         int shortArraySize = sData.length;
         byte[] outputBytes = new byte [shortArraySize * 2];
         for (int i = 0; i < shortArraySize; i++) {
